10400194ea6b305fe7f37f97948ac389472632bd
MAURICIO URIEL KOIFMAN <47066568@ort>
2023-03-27 15:32:05 -0300

    sin doff

--- h.js
+++ h.js
@@ -14,12 +14,13 @@ let AgregarAArr=(arr, valor)=>{
     console.log(arr);
 } 
 //////devuelve el último índice en el que un cierto elemento puede encontrarse en el arreglo
-let UltimoIndex=(arr, value)=>arr.lastIndexOf(value)
+let UltimoIndex=(arr, value)=>arr.lastIndexOf(value);
 /*Ejemplo: quiero buscar la última posición del string "hola" del siguiente array
 let arr2=["hola", "como va", "hola", "chao", "hola"]*/
 
 //////// mostrar por consola la sumatoria de todos los valores de un array de valores numericos
 let sumarArray = (arr) => arr.reduce((sum, point) => sum + point, 0);
 //inviertir el orden de los elementos de un array
-let InvertirOrdenARR=(arr)=>arr.reverse()
+let InvertirOrdenARR=(arr)=>arr.reverse();
 /////////// devolver los valores del array ordenados
+let ordenarArr=(arr)=> arr.sort();
